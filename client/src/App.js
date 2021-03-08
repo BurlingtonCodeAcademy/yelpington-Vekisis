d5da31f37d0edf9c09bbba04a6d1763ca07172a6
Vekisis <damianburbo@yahoo.com>
2021-03-08 09:06:30 -0500

    app.js note

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,12 @@
+//I was lazily putting this project off until after a family gathering this weekend, and then my grandfather died. I'll get back to this project another day.
+
 import logo from './logo.svg';
 import './App.css';
+
+//fetch restaurants or app.js in the backend
+
+//HTML goes down there
+//
 
 function App() {
   return (
